a8429f2189e48937d896fa3133843b0736ba1877
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-09-01 15:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const textInput = document.getElementById('text')
+const delayInput = document.getElementById('delay')
+const submitButton = document.getElementById('btn')
+const outputDiv = document.getElementById('output')
+
+async function showMessagewithDelay(){
+	const text= textInput.value;
+	const delay = parseInt(delayInput.value,10);
+
+	if(!text || isNaN(delay) || delay <= 0){
+		outputDiv.innerText = 'invalid input';
+		return;
+	}
+
+	await new  Promise(resolve => setTimeout(resolve,delay));
+	outputDiv.innerText = text;
+}
+
+submitButton.addEventListener('click',showMessagewithDelay)
